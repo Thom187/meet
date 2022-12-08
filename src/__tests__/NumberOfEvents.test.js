e80eb7c1679dcfd5eea8e8b4f70ca5fbc1379c26
Thom187 <tom.hoppe@gmx.de>
2022-12-08 13:30:05 +0100

    Add test 'change rendered number of events'

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -12,9 +12,18 @@ describe('<NumberOfEvents /> component', () => {
     expect(NumberOfEventsWrapper).toBeDefined();
   });
 
-  test('default number of event values is 32', () => {
+  test('default number of event value is 32', () => {
     expect(NumberOfEventsWrapper.find('input.num').prop('type')).toBe('number');
     expect(NumberOfEventsWrapper.state('num')).toBe(32);
+  });
+
+  test('user changes value for rendered number of events', () => {
+    expect(NumberOfEventsWrapper.state('num')).toBe(32);
+
+    NumberOfEventsWrapper.find('input.num').simulate('change', {
+      target: { value: 24 }
+    });
+    expect(NumberOfEventsWrapper.state('num')).toBe(24);
   });
 
 })
