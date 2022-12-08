7a952ae482963a65496e6cb4bf80a52e570c3e56
Thom187 <tom.hoppe@gmx.de>
2022-12-08 10:13:03 +0100

    Add test 'Render event summary'

--- src/Event.js
+++ src/Event.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
 
 class Event extends Component {
+
+  state = { collapsed: true };
+  toggleDetails = () => {
+    this.setState((prevState) => ({
+      collapsed: !prevState.collapsed,
+    }));
+  };
+
   render() {
+    const { event } = this.props;
+
     return (
       <div>
-
+        <h1 className='summary'>
+          {event.summary}
+        </h1>
       </div>
     );
   }
---
+++ src/__tests__/Event.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { mockData } from '../mock-data';
+import Event from '../Event';
+
+describe('Event /> component', () => {
+  let EventWrapper;
+  const event = mockData[0];
+  beforeAll(() => {
+    EventWrapper = shallow(<Event event={event} />);
+  });
+
+  test('renders the component', () => {
+    expect(EventWrapper).toBeDefined();
+  });
+
+  test('renders the summary as a h1', () => {
+    const summary = EventWrapper.find('h1.summary');
+    expect(summary).toHaveLength(1);
+    expect(summary.text()).toBe(event.summary);
+  });
+})
