ce7ff63a18adbd54e003b6dc6f73461ac8847850
Thom187 <tom.hoppe@gmx.de>
2022-12-09 13:04:30 +0100

    Add integration tests

--- src/App.js
+++ src/App.js
@@ -4,13 +4,43 @@ import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
 import { mockData } from './mock-data';
+import { extractLocations, getEvents } from './api';
 
 class App extends Component {
+  state = {
+    events: [],
+    locations: []
+  };
+
+  updateEvents = (location) => {
+    getEvents().then((events) => {
+      const locationEvents = (location === 'all') ?
+        events :
+        events.filter((event) => event.location === location);
+      this.setState({
+        events: locationEvents
+      });
+    });
+  }
+
+  componentDidMount() {
+    this.mounted = true;
+    getEvents().then((events) => {
+      if (this.mounted) {
+        this.setState({ events, locations: extractLocations(events) });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.mounted = false;
+  }
+
   render() {
     return (
       <div className="App">
-        <CitySearch />
-        <EventList events={mockData} />
+        <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
+        <EventList events={this.state.events} />
         <NumberOfEvents />
       </div>
     );
