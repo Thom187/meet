ba58f23e7aa4a977229c4e4f9ed074ab37092845
Thom187 <tom.hoppe@gmx.de>
2022-12-20 09:46:27 +0100

    Add ErrorAlert to NumberOfEvents component

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { ErrorAlert } from './Alert';
 
 class NumberOfEvents extends Component {
   state = { num: 32 };
@@ -6,7 +7,10 @@ class NumberOfEvents extends Component {
   changeNum = (value) => {
     this.setState({ num: value });
     this.props.updateNumberOfEvents(value);
-  }
+    if (value < 1 || value > 32) {
+      this.setState({ errorText: 'Please select a value between 1 and 32' });
+    } else this.setState({ errorText: '' });
+  };
 
   componentDidMount() {
     this.setState({ num: this.props.num || 32 });
@@ -16,18 +20,21 @@ class NumberOfEvents extends Component {
     const { num } = this.state;
 
     return (
-      <label>
-        Number of Events
-        <input
-          className='num'
-          type='number'
-          value={num}
-          onChange={(event) => {
-            this.changeNum(event.target.value);
-          }}
-        >
-        </input>
-      </label>
+      <div>
+        <ErrorAlert text={this.state.errorText} />
+        <label>
+          Number of Events
+          <input
+            className='num'
+            type='number'
+            value={num}
+            onChange={(event) => {
+              this.changeNum(event.target.value);
+            }}
+          >
+          </input>
+        </label>
+      </div>
     );
   }
 }
