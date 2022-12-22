e5ca667cb9457cf0bea31e4161205ecd17aa4763
Thom187 <tom.hoppe@gmx.de>
2022-12-22 12:02:20 +0100

    Updates

---
+++ precache-manifest.3ef03fc6e39845efe95389c3bf123945.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9e65eb1b06d05123d735",
+    "url": "/meet/static/css/main.50d71d51.chunk.css"
+  },
+  {
+    "revision": "9e65eb1b06d05123d735",
+    "url": "/meet/static/js/main.a479790c.chunk.js"
+  },
+  {
+    "revision": "dd4ea85d0d3f10afc010",
+    "url": "/meet/static/js/runtime~main.e38513b7.js"
+  },
+  {
+    "revision": "589294c76d0d3dd3b9ce",
+    "url": "/meet/static/js/2.0a7d0065.chunk.js"
+  },
+  {
+    "revision": "5b4d7cbf33e1065134f9",
+    "url": "/meet/static/js/3.7ff166bd.chunk.js"
+  },
+  {
+    "revision": "ae705c8487605e37a058b96a28653057",
+    "url": "/meet/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/meet/precache-manifest.b55b1b6cc12eef1484cd3cbd9dcaa461.js"
+  "/meet/precache-manifest.3ef03fc6e39845efe95389c3bf123945.js"
 );
 
 workbox.clientsClaim();
