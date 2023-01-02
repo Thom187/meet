85f7987afadc840909f0c5f19970dd5f82dfe190
Thom187 <tom.hoppe@gmx.de>
2023-01-02 11:23:30 +0100

    Updates

--- precache-manifest.9c8102acaf744080048d17e1c247c447.js
+++ precache-manifest.9c8102acaf744080048d17e1c247c447.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "1ecf075e164f5f2d869c",
+    "revision": "f87f4e14ff0b5e45df5a",
     "url": "/meet/static/css/main.187467ca.chunk.css"
   },
   {
-    "revision": "1ecf075e164f5f2d869c",
-    "url": "/meet/static/js/main.79a15cb1.chunk.js"
+    "revision": "f87f4e14ff0b5e45df5a",
+    "url": "/meet/static/js/main.0c0c534a.chunk.js"
   },
   {
     "revision": "3cf0eb1028a2502162df",
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/meet/static/js/3.e5d8b799.chunk.js"
   },
   {
-    "revision": "d91c50f696720a7af1fa679e9b41178f",
+    "revision": "bc3806d5cfde7700561c41dacd3d0f8f",
     "url": "/meet/index.html"
   }
 ];
