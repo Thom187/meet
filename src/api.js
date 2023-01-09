43846813c647127b0491bafc8cf30de1f656380d
Thom187 <tom.hoppe@gmx.de>
2023-01-09 16:56:19 +0100

    Fix no-useless-concat warning

--- src/api.js
+++ src/api.js
@@ -66,7 +66,7 @@ const removeQuery = () => {
 const getToken = async (code) => {
   const encodeCode = encodeURIComponent(code);
   const { access_token } = await fetch(
-    'https://aimss6nswg.execute-api.eu-central-1.amazonaws.com/dev/api/token' + '/' + encodeCode
+    `https://aimss6nswg.execute-api.eu-central-1.amazonaws.com/dev/api/token/${encodeCode}`
   )
     .then((res) => {
       return res.json();
@@ -96,7 +96,7 @@ export const getEvents = async () => {
 
   if (token) {
     removeQuery();
-    const url = 'https://aimss6nswg.execute-api.eu-central-1.amazonaws.com/dev/api/get-events' + '/' + token;
+    const url = `https://aimss6nswg.execute-api.eu-central-1.amazonaws.com/dev/api/get-events/${token}`;
     const result = await axios.get(url);
     if (result.data) {
       var locations = extractLocations(result.data.events);
